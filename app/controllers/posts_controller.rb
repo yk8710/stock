ab58b60f6b58ed51407b08d5c0236ca67cc01ec9
yk8710 <y.kaneko198710@gmail.com>
2022-08-11 23:24:26 +0900

    投稿編集機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.where(user_id: current_user.id).includes(:user).order("created_at DESC")
@@ -25,14 +26,25 @@ class PostsController < ApplicationController
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
   end
 
   def destroy
   end
 
   private
+
   def post_params
     params.require(:post).permit(:name, :best_before, :quantity, :location, :category_id).merge(user_id:current_user.id)
   end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 
 end
