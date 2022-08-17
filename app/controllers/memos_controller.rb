6290e532126e90048b0d941c29de46e1d0fc3601
yk8710 <y.kaneko198710@gmail.com>
2022-08-17 15:39:18 +0900

    メモ投稿機能実装

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -2,19 +2,33 @@ class MemosController < ApplicationController
   before_action :authenticate_user!
 
   def index
-  end
-
-  def new
+    @memos = Memo.where(user_id: current_user.id).includes(:user).order("created_at DESC")
+    @memo = Memo.new
   end
 
   def create
+    @memo = Memo.new(memo_params)
+    if @memo.save
+      redirect_to memos_path
+    else
+      redirect_to memos_path
+    end
   end
 
   def edit
   end
 
+  def update
+  end
+
   def destroy
   end
 
+  private
 
+  def memo_params
+    params.require(:memo).permit(:name, :contents).merge(user_id: current_user.id)
+  end
+
+  
 end
