ee3cc0ef37646d173f85b4aa6553b6330b81a022
yk8710 <y.kaneko198710@gmail.com>
2022-08-19 15:38:14 +0900

    楽天apiのgem追加
    発注画面に関するコントローラー、ルーティング、ビューファイルも作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'goods/search'
   devise_for :users
   root to: "top#index"
   resources :posts do
@@ -9,4 +10,5 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   resources :memos
   get '/post/category', to: "posts#category"
+  get '/goods/search'
 end
