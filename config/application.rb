d61dc941750f999c404c0706c98f07bdd5b87bfa
yk8710 <y.kaneko198710@gmail.com>
2022-08-31 14:44:53 +0900

    commit11

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,6 @@ module Stock
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
-    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
