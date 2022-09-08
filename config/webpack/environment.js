c3eba9618c72251635e087c61726424693174bc3
yk8710 <y.kaneko198710@gmail.com>
2022-09-08 22:08:19 +0900

    commit32

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,34 +1,34 @@
 const { environment } = require('@rails/webpacker');
 
 
-const webpack = require('webpack');
-environment.plugins.prepend(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-  })
-);
+// const webpack = require('webpack');
+// environment.plugins.prepend(
+//   'Provide',
+//   new webpack.ProvidePlugin({
+//     $: 'jquery',
+//     jQuery: 'jquery',
+//   })
+// );
 
-const customConfig = {
-  resolve: {
-    fallback: {
-      dgram: false,
-      fs: false,
-      net: false,
-      tls: false,
-      child_process: false
-    }
-  }
-};
+// const customConfig = {
+//   resolve: {
+//     fallback: {
+//       dgram: false,
+//       fs: false,
+//       net: false,
+//       tls: false,
+//       child_process: false
+//     }
+//   }
+// };
 
-environment.config.delete('node.dgram')
-environment.config.delete('node.fs')
-environment.config.delete('node.net')
-environment.config.delete('node.tls')
-environment.config.delete('node.child_process')
+// environment.config.delete('node.dgram')
+// environment.config.delete('node.fs')
+// environment.config.delete('node.net')
+// environment.config.delete('node.tls')
+// environment.config.delete('node.child_process')
 
-environment.config.merge(customConfig);
+// environment.config.merge(customConfig);
 
 
 module.exports = environment;
